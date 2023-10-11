1341e2bfb98b9751923eb870e5e274b6c265c14b
bkgriffin <4809837+bkgriffin@users.noreply.github.com>
2023-10-11 10:31:16 -0400

    Added the ability to create a user-defined grid.

--- etch-a-sketch.js
+++ etch-a-sketch.js
@@ -40,4 +40,31 @@ function addHoverEffect(div) {
     div.style.backgroundColor = "black";
 }
 
+// Add an event listener for the 'Generate Grid' button.
+function addGenerateGridButtonEventListener() {
+    const generateGridButton = document.querySelector(".generate-button");
+    generateGridButton.addEventListener('click', function() { createUserDefinedGrid() });
+}
+
+// Create the Etch-a-Sketch grid using user-defined values.
+function createUserDefinedGrid() {
+    const xAxis = document.querySelector(".x-axis");
+    const yAxis = document.querySelector(".y-axis");
+    deleteGrid();
+    createGrid(xAxis.value, yAxis.value);
+}
+
+// Delete the squares inside of the Etch-a-Sketch grid.
+function deleteGrid() {
+    const containerDiv = document.querySelector(".container");
+    containerDiv.replaceChildren();
+}
+
+// TODO: 
+// - Style the x-axis/y-axis/button elements
+// - Check x-axis and y-axis inputs for valid input (1-100, no decimal numbers)
+// - Different pen color? 
+// - In rare instances, the number of x/y input to generate grid will cause the squares to wrap.
+
+addGenerateGridButtonEventListener();
 createGrid(10, 10);
